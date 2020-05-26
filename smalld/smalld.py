4fa156a1bbcaac5d98a793a2d83a4da230d4418a
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-05-27 00:03:45 +0200

    Use explicit intent flag values instead of auto

--- smalld/smalld.py
+++ smalld/smalld.py
@@ -96,22 +96,21 @@ class SmallD:
 
 
 class Intent(Flag):
-    # Gateway intents https://discord.com/developers/docs/topics/gateway#gateway-intents
-    GUILDS = auto()
-    GUILD_MEMBERS = auto()
-    GUILD_BANS = auto()
-    GUILD_EMOJIS = auto()
-    GUILD_INTEGRATIONS = auto()
-    GUILD_WEBHOOKS = auto()
-    GUILD_INVITES = auto()
-    GUILD_VOICE_STATES = auto()
-    GUILD_PRESENCES = auto()
-    GUILD_MESSAGES = auto()
-    GUILD_MESSAGE_REACTIONS = auto()
-    GUILD_MESSAGE_TYPING = auto()
-    DIRECT_MESSAGES = auto()
-    DIRECT_MESSAGE_REACTIONS = auto()
-    DIRECT_MESSAGE_TYPING = auto()
+    GUILDS = 1 << 0
+    GUILD_MEMBERS = 1 << 1
+    GUILD_BANS = 1 << 2
+    GUILD_EMOJIS = 1 << 3
+    GUILD_INTEGRATIONS = 1 << 4
+    GUILD_WEBHOOKS = 1 << 5
+    GUILD_INVITES = 1 << 6
+    GUILD_VOICE_STATES = 1 << 7
+    GUILD_PRESENCES = 1 << 8
+    GUILD_MESSAGES = 1 << 9
+    GUILD_MESSAGE_REACTIONS = 1 << 10
+    GUILD_MESSAGE_TYPING = 1 << 11
+    DIRECT_MESSAGES = 1 << 12
+    DIRECT_MESSAGE_REACTIONS = 1 << 13
+    DIRECT_MESSAGE_TYPING = 1 << 14
 
     @staticmethod
     def all():
