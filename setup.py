72b3ebaf9da43d570a557656c3ed52fd2cd5f893
ianagbip1oti <ianagbip1oti@gmail.com>
2020-06-04 02:05:59 +0000

    📖 DOC: Add readme to setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
+import os
+
 from setuptools import setup
+
+
+def readme():
+    readme_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+
+    with open(readme_path, encoding="utf-8") as f:
+        return f.read()
+
 
 setup(
     name="smalld",
+    description="A minimalist python client for the Discord API",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     packages=["smalld", "smalld.resources"],
     package_data={"smalld.resources": ["*"]},
     use_scm_version=True,
