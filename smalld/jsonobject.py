f162ebb86767304dde8a6a391578242b264e3157
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-06-18 21:02:46 +0200

    🐛 FIX: Correctly map nested structures by using JsonArray instead of tuple

--- smalld/jsonobject.py
+++ smalld/jsonobject.py
@@ -1,19 +1,22 @@
 from collections import abc
 
 
+def wrap_value(value):
+    if isinstance(value, dict):
+        return JsonObject(value)
+    if isinstance(value, list):
+        return JsonArray(value)
+    return value
+
+
 class JsonObject(abc.Mapping):
-    __slots__ = ("__data",)
+    __slots__ = "__data",
 
     def __init__(self, data):
         self.__data = data
 
     def __getitem__(self, key):
-        value = self.__data[key]
-        if isinstance(value, dict):
-            value = JsonObject(value)
-        elif isinstance(value, list):
-            value = tuple(value)
-        return value
+        return wrap_value(self.__data[key])
 
     def __iter__(self):
         return iter(self.__data)
@@ -28,6 +31,22 @@ class JsonObject(abc.Mapping):
             raise AttributeError(
                 f"'{type(self).__name__}' object has no attribute '{attr}'"
             ) from None
+
+    def __repr__(self):
+        return repr(self.__data)
+
+
+class JsonArray(abc.Sequence):
+    __slots__ = "__data",
+
+    def __init__(self, data):
+        self.__data = data
+    
+    def __getitem__(self, index):
+        return wrap_value(self.__data[index])
+
+    def __len__(self):
+        return len(self.__data)
 
     def __repr__(self):
         return repr(self.__data)
